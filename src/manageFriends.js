a353310baa7f1d28a8f689caac91d000e8d567a3
Sergio Berdeja <sergioberdeja@gmail.com>
2022-08-27 18:44:18 -0600

    Completed assignment

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,21 @@
-export function manageFriends(state, action) {
-  // your code here
+export function manageFriends(state = { friends: [] }, action) {
+  let updatedFriends = [...state.friends]
+  switch (action.type) {
+    case "friends/add":
+      return ({
+        friends: [...updatedFriends,
+        {
+          name: action.payload.name,
+          hometown: action.payload.hometown,
+          id: action.payload.id
+        }
+        ]
+      })
+    case "friends/remove":
+      return ({
+    friends: updatedFriends.filter(friend => friend.id !== action.payload)
+    })
+    default:
+      return state
+  }
 }
